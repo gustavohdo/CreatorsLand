967254e8ff606f588a3fef22eb3a4647e61f4a8c
Gustavo <gustavohenriqueoliveir@gmail.com>
2024-04-04 21:39:57 -0300

    Criação do componente event-card

--- EventsList/src/app/app.module.ts
+++ EventsList/src/app/app.module.ts
@@ -8,6 +8,7 @@ import { EventsBannerComponent } from './events-banner/events-banner.component';
 import { HomeComponent } from './home/home.component';
 import { EventsPageComponent } from './events-page/events-page.component';
 import { ComunityPageComponent } from './comunity-page/comunity-page.component';
+import { EventCardComponent } from './event-card/event-card.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { ComunityPageComponent } from './comunity-page/comunity-page.component';
     EventsBannerComponent,
     HomeComponent,
     EventsPageComponent,
-    ComunityPageComponent
+    ComunityPageComponent,
+    EventCardComponent
   ],
   imports: [
     BrowserModule,
